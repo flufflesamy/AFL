13c22fd6ec82828ace0177840c3df11e0b65c1ca
Amy Poon <amy@fluffles.me>
2025-06-21 11:54:52 -0400

    rename maps to missions

--- addons/missions/script_component.hpp
+++ addons/missions/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT maps
-#define COMPONENT_BEAUTIFIED Maps
+#define COMPONENT missions
+#define COMPONENT_BEAUTIFIED Missions
 
 #include "\z\afl\addons\main\script_mod.hpp"
 #include "\z\afl\addons\main\script_macros.hpp"
