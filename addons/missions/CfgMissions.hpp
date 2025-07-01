2f8930ed991defff46e5833bce3e352b240b888a
Amy Poon <amy@fluffles.me>
2025-07-01 16:09:36 -0400

    try and fix mission loading

--- addons/missions/CfgMissions.hpp
+++ addons/missions/CfgMissions.hpp
@@ -1,7 +1,7 @@
 class CfgMissions {
     class MPMissions {
         class Selkie_FiringRange {
-            directory = "Selkie_FiringRange.Stratis";
+            directory = "z\afl\addons\missions\Selkie_FiringRange.Stratis";
             briefingName = "Selkie Firing Range";
         };
     };
--- addons/missions/Selkie_FiringRange.Stratis/configs/CfgSettings.hpp
+++
@@ -1,9 +0,0 @@
-class CfgSettings {
-    class CBA {
-        class Caching {
-            compile = 0;
-            xeh = 0;
-            functions = 0;
-        };
-    };
-};
