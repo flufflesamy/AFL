69f0ccc01ddcc03bf810c0c51f2c3f67c29e7d32
Amy Poon <amy@amypoon.me>
2025-09-18 14:19:34 -0400

    MAIDView scaffolding

---
+++ addons/maidview/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "afl_main"
+        };
+        author = "flufflesamy";
+        VERSION_CONFIG;
+    };
+};
---
+++ addons/maidview/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT maidview
+#define COMPONENT_BEAUTIFIED MAIDView
+
+#include "\z\afl\addons\main\script_mod.hpp"
+#include "\z\afl\addons\main\script_macros.hpp"
